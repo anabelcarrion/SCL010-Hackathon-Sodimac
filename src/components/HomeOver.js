47e0799a40829d0db1a8d2a887a7bf000f17959b
dacoquima <danimalistica@gmail.com>
2020-01-06 16:09:57 -0300

    first commit

--- src/components/HomeOver.js
+++ src/components/HomeOver.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import './HomeOver.css'
-import Logo  from '../img/logo.png'
+import Logo from '../img/logo.png'
 
 class Home extends Component {
   render() {
     return (
         <div class="homeOver">
-            <img src= { Logo } alt="Big Burger" className="bigBurger"></img>        
+            <img src= { Logo } alt="logoSodimac" className="logoSodimac"></img>        
         </div>
     )
   }}
